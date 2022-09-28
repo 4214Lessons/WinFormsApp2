a3bc98922275df95dd5483fe1b59eb1d5d758474
Tural Novruzov <t.novruzov@itstep.edu.az>
2022-09-28 16:45:49 +0400

    Add Form2

---
+++ Source/Form2.cs
@@ -0,0 +1,9 @@
+﻿namespace Source;
+
+public partial class Form2 : Form
+{
+    public Form2()
+    {
+        InitializeComponent();
+    }
+}
--- Source/Program.cs
+++ Source/Program.cs
@@ -6,6 +6,6 @@ internal static class Program
     static void Main()
     {
         ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
+        Application.Run(new Form2());
     }
 }
