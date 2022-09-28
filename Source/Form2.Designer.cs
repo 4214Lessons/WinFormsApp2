299dd57f23d034ff2429169d5d9f1ef1528be5d5
Tural Novruzov <t.novruzov@itstep.edu.az>
2022-09-28 17:49:13 +0400

    last commit

--- Source/Form2.Designer.cs
+++ Source/Form2.Designer.cs
@@ -28,20 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.btn_print = new System.Windows.Forms.Button();
+            this.gBox_Colors = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.gBox_Payment = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.gBox_Colors.SuspendLayout();
+            this.gBox_Payment.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox1.Location = new System.Drawing.Point(12, 100);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 34);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Blue";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox2.Location = new System.Drawing.Point(12, 60);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(67, 34);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "Red";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.checkBox3.FlatAppearance.BorderSize = 5;
+            this.checkBox3.FlatAppearance.CheckedBackColor = System.Drawing.Color.Blue;
+            this.checkBox3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox3.Location = new System.Drawing.Point(12, 20);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(87, 34);
+            this.checkBox3.TabIndex = 0;
+            this.checkBox3.Text = "Green";
+            this.checkBox3.UseVisualStyleBackColor = false;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox_CheckedChanged);
+            // 
+            // btn_print
+            // 
+            this.btn_print.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn_print.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_print.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_print.Location = new System.Drawing.Point(12, 182);
+            this.btn_print.Name = "btn_print";
+            this.btn_print.Size = new System.Drawing.Size(110, 41);
+            this.btn_print.TabIndex = 2;
+            this.btn_print.Text = "Print";
+            this.btn_print.UseVisualStyleBackColor = false;
+            this.btn_print.Click += new System.EventHandler(this.btn_print_Click);
+            // 
+            // gBox_Colors
+            // 
+            this.gBox_Colors.Controls.Add(this.checkBox1);
+            this.gBox_Colors.Controls.Add(this.checkBox2);
+            this.gBox_Colors.Controls.Add(this.checkBox3);
+            this.gBox_Colors.Location = new System.Drawing.Point(12, 12);
+            this.gBox_Colors.Name = "gBox_Colors";
+            this.gBox_Colors.Size = new System.Drawing.Size(110, 154);
+            this.gBox_Colors.TabIndex = 4;
+            this.gBox_Colors.TabStop = false;
+            this.gBox_Colors.Text = "Colors";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton1.Location = new System.Drawing.Point(31, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 34);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Visa";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton2.Location = new System.Drawing.Point(31, 57);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(138, 34);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "MasterCard";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton3.Location = new System.Drawing.Point(31, 97);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(90, 34);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "PayPal";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // gBox_Payment
+            // 
+            this.gBox_Payment.Controls.Add(this.radioButton3);
+            this.gBox_Payment.Controls.Add(this.radioButton1);
+            this.gBox_Payment.Controls.Add(this.radioButton2);
+            this.gBox_Payment.Location = new System.Drawing.Point(588, 18);
+            this.gBox_Payment.Name = "gBox_Payment";
+            this.gBox_Payment.Size = new System.Drawing.Size(200, 148);
+            this.gBox_Payment.TabIndex = 6;
+            this.gBox_Payment.TabStop = false;
+            this.gBox_Payment.Text = "Payment";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(290, 36);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(284, 142);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 9;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.gBox_Payment);
+            this.Controls.Add(this.gBox_Colors);
+            this.Controls.Add(this.btn_print);
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
+            this.gBox_Colors.ResumeLayout(false);
+            this.gBox_Colors.PerformLayout();
+            this.gBox_Payment.ResumeLayout(false);
+            this.gBox_Payment.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private Button btn_print;
+        private GroupBox gBox_Colors;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private GroupBox gBox_Payment;
+        private DateTimePicker dateTimePicker1;
+        private Button button1;
+        private MonthCalendar monthCalendar1;
     }
 }
--- Source/Form2.cs
+++ Source/Form2.cs
@@ -6,4 +6,30 @@ public partial class Form2 : Form
     {
         InitializeComponent();
     }
+
+    private void btn_print_Click(object sender, EventArgs e)
+    {
+        foreach (Control control in gBox_Colors.Controls)
+        {
+            if(control is CheckBox chk && chk.Checked)
+            {
+                MessageBox.Show(chk.Text);
+            }
+        }
+    }
+
+    private void checkBox_CheckedChanged(object sender, EventArgs e)
+    {
+        // var chk = sender as CheckBox;
+        // MessageBox.Show($"{chk?.Text}  {chk?.Checked}");
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        //  MessageBox.Show(dateTimePicker1.Value.ToString());
+
+        // MessageBox.Show(monthCalendar1.SelectionStart.ToString());
+        // MessageBox.Show(monthCalendar1.SelectionEnd.ToString());
+
+    }
 }
